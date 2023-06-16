b403871b34e185346b55e0ef11c66041bd0fef4d
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-06-16 10:32:36 +0100

    Initial commit

---
+++ src/main/java/luqmanmohammad/U2D15DesignPatterns/U2D15DesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package luqmanmohammad.U2D15DesignPatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2D15DesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2D15DesignPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/luqmanmohammad/U2D15DesignPatterns/U2D15DesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package luqmanmohammad.U2D15DesignPatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2D15DesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
